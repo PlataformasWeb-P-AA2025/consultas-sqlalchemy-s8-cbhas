bb79c90a49a96755a0781f5db6745957cfb33774
Sebastia <calderonsebastian211@gmail.com>
2025-05-27 09:06:44 -0500

    Consulta 2

--- consulta2.py
+++ consulta2.py
@@ -8,12 +8,13 @@ session = Session()
 
 departamentos = (
     session.query(Departamento)
-    .join(Departamento.cursos)   # Une departamentos con cursos
-    .join(Curso.tareas)           # Une cursos con tareas
-    .join(Tarea.entregas)         # Une tareas con entregas
-    .filter(Entrega.calificacion <= 0.3)
+    .join(Departamento.cursos)   # Une departamentos con cursos porque un departamento tiene varios cursos
+    .join(Curso.tareas)           # Une cursos con tareas porque un curso tiene varias tareas de un departamento
+    .join(Tarea.entregas)         # Une tareas con entregas porque una tarea tiene varias entregas 
+    .filter(Entrega.calificacion <= 0.3) # Filtra las entregas con calificación menor o igual a 0.3
     .all()
 )
 
+# Imprime el nombre del departamento y el número de cursos asociados
 for d in departamentos:
     print(f"Departamento: {d.nombre} | Número de cursos: {len(d.cursos)}")
